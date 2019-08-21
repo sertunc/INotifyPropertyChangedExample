9a006404e398abf597f736bab7cd3c6fcc88ee2f
cahitsertunc.selen <cahitsertunc.selen@CSSELEN>
2019-08-21 16:51:57 +0300

    Add project files.

---
+++ INotifyPropertyChangedExample/Banka.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace INotifyPropertyChangedExample
+{
+    public class Banka : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private double para;
+
+        public double Para
+        {
+            get => para;
+            set
+            {
+                para = value;
+
+                NotifyPropertyChanged();
+            }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ INotifyPropertyChangedExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace INotifyPropertyChangedExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Banka banka = new Banka();
+            banka.PropertyChanged += Banka_PropertyChanged;
+            banka.Para += 100;
+
+            Console.Read();
+        }
+
+        private static void Banka_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            Console.WriteLine("Para eklendi");
+        }
+    }
+}
